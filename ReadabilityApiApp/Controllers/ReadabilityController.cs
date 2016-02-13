651e8687184c7259df79a70c9a4590ec76ba95aa
Frank Boucher <fboucher@outlook.com>
2016-02-13 16:55:09 -0500

    - Add The default Get method.
    - Exclude all private settings from the Repo
    - Enable swaggerUI

--- ReadabilityApiApp/Controllers/ReadabilityController.cs
+++ ReadabilityApiApp/Controllers/ReadabilityController.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using CSharp.Readability.Api.Interfaces;
+using CSharp.Readability.Api.Models;
+using CSharp.Readability.Connect;
+using Spring.Social.OAuth1;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,5 +14,28 @@ namespace ReadabilityApiApp.Controllers
 {
     public class ReadabilityController : ApiController
     {
+        // Configure the Callback URL
+        readonly IOAuth1ServiceProvider<IReadability> _readabilityProvider = new ReadabilityServiceProvider(ConfigurationManager.AppSettings["ReadabilityApiKey"], ConfigurationManager.AppSettings["ReadabilityApiSecret"]);
+        private IReadability _realAPI;
+
+        private IReadability RealAPI
+        {
+            get
+            {
+                if (_realAPI == null)
+                {
+                    _realAPI = _readabilityProvider.GetApi(ConfigurationManager.AppSettings["AccessToken"], ConfigurationManager.AppSettings["Secret"]);
+                }
+                return _realAPI;
+            }
+        }
+
+        [Route("GetAll")]
+        [HttpGet]
+        public IEnumerable<BookmarkDetails> GetAll()
+        {
+            var bookmarks = RealAPI.BookmarkOperations.GetReadingListBookmarksAsync(1, 50, "-date_added", "").Result;
+            return bookmarks.Bookmarks;
+        }
     }
 }
